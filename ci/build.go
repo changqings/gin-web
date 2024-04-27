9c5b351c68f0bb7a2cc9c4bb3c9b9c4b4ecc8c83
shenchangqing <changqing.shen@tencalss.com>
2024-04-28 02:49:22 +0800

    update

--- ci/build.go
+++ ci/build.go
@@ -1,1 +1,5 @@
 package ci
+
+// run in docker and only on linux, code on ubuntu
+// 过程：代码仓库(gitlab_on_docker) --> 挂载到特定容器(docker local) --> 构建并生成产物(use Dockerfile)
+// --> 构建镜像并上传到镜像仓库(docker-registry)
