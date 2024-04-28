1ac7ccfb4f47ac2d62f525393fab72ede3c4fca4
changqing_shen <shenqinpeng@126.com>
2024-04-29 02:58:44 +0800

    Update build.go

--- ci/build.go
+++ ci/build.go
@@ -15,7 +15,7 @@ import (
 // 过程：代码仓库(gitlab_on_docker) --> 挂载到特定容器(docker local) --> 构建并生成产物(use Dockerfile)
 // --> 构建镜像并上传到镜像仓库(docker-registry)
 
-// gitlab_admin_read_all token=glpat-Arnoz3_yrcDhZyVgYt9j
+
 // gitlab_host = http://192.168.1.15
 
 // ssh://git@gitlab.scq.com:522/devops/go-ws.git
